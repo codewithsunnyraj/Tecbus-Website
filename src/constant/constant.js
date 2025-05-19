ed6d81075d25500175693efae9eb2338d08d15bb
Skulldued <skulldued30@gmail.com>
2025-05-20 00:18:38 +0530

    First commit

---
+++ src/constant/constant.js
@@ -0,0 +1,111 @@
+import logo from "../assets/logo-white.png";
+import testimonial from "../assets/testimonial.png";
+import hero from "../assets/about-thumb1_1.jpg";
+import left_build from "../assets/about-thumb1_1.jpg";
+import deal from "../assets/pricing-thumb1_1.png";
+import orange from "../assets/orange-bg.png";
+import white from "../assets/cta-shape1_1.png";
+import footer from "../assets/cta-thumb1_1.png";
+import cloud from "../assets/cloud-right.png";
+import cloud1 from "../assets/cloud.png";
+import development from "../assets/development.png";
+import git from "../assets/git.png";
+import figma from "../assets/figma.png";
+import managed from "../assets/managed.png";
+import test1 from "../assets/test1.png";
+import test2 from "../assets/test2.png";
+import risk from "../assets/risk.png";
+import testimonial2 from "../assets/testimonial2.png";
+import testimonial3 from "../assets/testimonial3.png";
+import wordpress from "../assets/wordpress.png";
+import location from "../assets/location.png";
+
+export const assets = {
+  logo,
+  cloud1,
+  managed,
+  development,
+  git,
+  figma,
+  test1,
+  test2,
+  risk,
+  testimonial2,
+  testimonial3,
+  wordpress,
+  location,
+  testimonial,
+  hero,
+  left_build,
+  deal,
+  orange,
+  white,
+  footer,
+};
+
+export const services = [
+  {
+    image: cloud,
+    title: "Cloud Migration Services",
+    description:
+      "Easily create reusable payment links that you can share with your customers",
+  },
+  {
+    image: cloud,
+    title: "IT Audits & Compliance",
+    description:
+      "Easily create reusable payment links that you can share with your customers",
+  },
+  {
+    image: cloud,
+    title: "Custom Development",
+    description:
+      "Easily create reusable payment links that you can share with your customers",
+  },
+  {
+    image: cloud,
+    title: "Virtualization Services",
+    description:
+      "Easily create reusable payment links that you can share with your customers",
+  },
+  {
+    image: cloud,
+    title: "Data Center Management",
+    description:
+      "Easily create reusable payment links that you can share with your customers",
+  },
+  {
+    image: cloud,
+    title: "Risk Assessment",
+    description:
+      "Easily create reusable payment links that you can share with your customers",
+  },
+];
+
+export const faq = [
+  {
+    title: "What types of businesses do you support?",
+    description:
+      "We work with businesses of all sizes, from startups to enterprises, across various industries. Our IT solutions are customized to meet the unique needs of each client.",
+  },
+  {
+    title: "How quickly can we get support if an issue arises?",
+    description:
+      "Car service is essential for maintaining the performance and maintaining the performance and longevity Car service is essential for maintaining",
+  },
+  {
+    title: "Do you offer scalable solutions as our business grows?",
+    description:
+      "Car service is essential for maintaining the performance and maintaining the performance and longevity Car service is essential for maintaining",
+  },
+  {
+    title: "How secure is our data with your services?",
+    description:
+      "Car service is essential for maintaining the performance and maintaining the performance and longevity Car service is essential for maintaining",
+  },
+  {
+    title: "Can you help with cloud migration?",
+    description:
+      "Car service is essential for maintaining the performance and maintaining the performance and longevity Car service is essential for maintaining",
+  },
+];
